ee7dd8ddee05c9a6053a444f499235176bfa1899
김동균 <165386023+d6nggyun@users.noreply.github.com>
2024-11-27 17:17:58 +0900

    Add files via upload

---
+++ src/test/java/kkpod/ToyProject/ToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kkpod.ToyProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
