bf5d9a241d7ec79712e987c85cfbf651135a9c99
Oh-jj <fvjung01@gmail.com>
2025-01-18 00:39:09 +0900

    [Feat] 댓글 repository 설계 #10

--- src/main/java/kpol/Inventory/domain/comment/repository/CommentRepository.java
+++ src/main/java/kpol/Inventory/domain/comment/repository/CommentRepository.java
@@ -1,12 +1,13 @@
 package kpol.Inventory.domain.comment.repository;
 
 import kpol.Inventory.domain.comment.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-public class CommentRepository {
-    // 게시글의 모든 댓글 조회
-    List<Comment> findAllByBoardId(Long boardId);
-    // 댓글의 모든 대댓글 조회
-    List<Comment> findAllByParentCommentId(Long ParentCommentId);
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByBoardId(Long boardId);
+    List<Comment> findByParentCommentId(Long parentCommentId);
 }
