e31bdd70a8472bf06a32a43e9eef59ca134ccc7f
Oh-jj <fvjung01@gmail.com>
2025-01-18 00:06:55 +0900

    [Fix] 댓글 dto 설계 #9

--- src/main/java/kpol/Inventory/domain/comment/dto/req/CommentRequestDto.java
+++ src/main/java/kpol/Inventory/domain/comment/dto/req/CommentRequestDto.java
@@ -1,13 +1,12 @@
 package kpol.Inventory.domain.comment.dto.req;
 
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class CommentRequestDto {
-    private String content;
-    private Long boardId;
-    private Long memberId; // 댓글 작성자
-    private Long parentCommentId; // 부모 댓글 (없으면 null)
+    private String content;  // 댓글 내용 저장
+    private Long parentCommentId; // 대댓글 작성시, 부모 댓글의 id 저장 (단, 일반 댓글인 경우 =null)
 }
--- src/main/java/kpol/Inventory/domain/comment/dto/res/CommentResponseDto.java
+++ src/main/java/kpol/Inventory/domain/comment/dto/res/CommentResponseDto.java
@@ -1,19 +1,35 @@
 package kpol.Inventory.domain.comment.dto.res;
 
 
-import lombok.Builder;
+import kpol.Inventory.domain.comment.entity.Comment;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
-@Builder
+
 
 public class CommentResponseDto {
-    private Long id;
-    private String content;
-    private String authorName; // 작성자 이름
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-    private Long parentCommentId; // 부모 댓글 ID
+    private Long id; //댓글 고유 id
+    private String content; // 댓글 내용
+    private LocalDateTime createdAt; //댓글 생성 시간
+    private LocalDateTime updatedAt; //댓글 수정 시간
+    private Long memberId; // 댓글 작성자의 id
+    private Long boardId; // 댓글이 있는 게시물의 id
+    private Long parentCommentId; // 부모 댓글의 id (대댓글인 경우)
+    private List<CommentResponseDto> replies; // 대댓글 목록 저장 (재귀?)
+
+    public CommentResponseDto(Long id, String content, LocalDateTime createdAt, LocalDateTime updatedAt
+    ,Long memberId, Long boardId, Long parentCommentId, List<CommentResponseDto> replies) {
+        this.id = id;
+        this.content = content;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.memberId = memberId;
+        this.boardId = boardId;
+        this.parentCommentId = parentCommentId;
+        this.replies = replies;
+    }
 }
